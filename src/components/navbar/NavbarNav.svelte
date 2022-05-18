6f89e1c92450ac6329f79395c50776d99ecf0613
teely44y <ju.tannatee2543@gmail.com>
2022-05-18 20:48:00 +0700

    remove: unused link ❌

--- src/components/navbar/NavbarNav.svelte
+++ src/components/navbar/NavbarNav.svelte
@@ -4,5 +4,4 @@
 
 <ul class="flex flex-row justify-start space-x-4 bg-gradient-to-r from-black via-black to-indigo-900 shadow-md py-4 px-8">
   <NavbarItem title="Home" href="/" />
-  <NavbarItem title="Discord" href="/discord" />
 </ul>
