bcc4a264c2f3dc2533ee066eba1586bff2af16f6
teely44y <ju.tannatee2543@gmail.com>
2022-05-19 23:13:56 +0700

    add: error page handler.

---
+++ src/routes/__error.svelte
@@ -0,0 +1,26 @@
+<script context="module" lang="ts">
+  export function load({ error, status }: { error: any; status: number }) {
+    return {
+      props: {
+        status: status,
+        error: error.message,
+      }
+    }
+  }
+</script>
+
+<script lang="ts">
+  import Meta from "../components/Meta.svelte";
+  import type { MetaProps } from '../interfaces/MetaProps';
+
+  export let status: number;
+  export let error: string;
+
+  const metadata: MetaProps = {
+    title: "404 NOT FOUND",
+    description: "page not found or deleted."
+  }
+</script>
+
+<Meta { metadata } />
+<h1>{ status }: { error }</h1>
