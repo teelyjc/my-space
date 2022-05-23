5b80219e0bf99bc30c2ad4afec833db9547c89ed
teely44y <ju.tannatee2543@gmail.com>
2022-05-23 20:04:11 +0700

    refactor: setting up for node, docker

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,7 +10,7 @@ export default {
 	}),
 
 	kit: {
-		adapter: vercel(),
+		adapter: adapter(),
 		prerender: {
 			default: true,
 		}
